aea5fefbcea616e5a17e6c3d5a6a3209dd909620
Perrin Cloutier <perrincloutier@Perrins-MacBook-Pro.local>
2017-11-29 23:31:46 -0500

    light flares in center

--- AnimationIsand/AnimationScene.swift
+++ AnimationIsand/AnimationScene.swift
@@ -13,6 +13,7 @@ class AnimationScene: SKScene {
     
     var animationBackground: SKSpriteNode!
     var colors = [UIColor]()
+    var bubbleShouldGrow: Bool = false
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
@@ -24,7 +25,18 @@ class AnimationScene: SKScene {
         colors.append(UIColor.red)
         colors.append(UIColor.green)
         colors.append(UIColor.blue)
-        animationBackground = SKSpriteNode(color: UIColor.blue, size: size)
+        colors.append(UIColor.yellow)
+        colors.append(UIColor.purple)
+        colors.append(UIColor.orange)
+        let vintColors = Colors.getVintage()
+        let psyColors = Colors.getPsychedelicIceCreamShopColors()
+        for x in vintColors {
+            colors.append(x)
+        }
+        for x in psyColors {
+            colors.append(x)
+        }
+        animationBackground = SKSpriteNode(color: UIColor.clear, size: size)
         animationBackground.anchorPoint = CGPoint(x: 0, y: 1.0)
         animationBackground.position = CGPoint(x: 0, y: 0)
         self.addChild(animationBackground)
@@ -38,23 +50,45 @@ class AnimationScene: SKScene {
     
     func addBubble() {
 //        let bubble = SKSpriteNode(color: UIColor.white, size: CGSize(width: 10, height: 10))
-        let bubble = SKShapeNode(circleOfRadius: CGFloat(arc4random_uniform(40)))
-        bubble.fillColor = colors[Int(arc4random_uniform(2))]
+        let bubble = SKShapeNode(circleOfRadius: CGFloat(arc4random_uniform(140)))
+    
+        bubble.fillColor = colors[Int(arc4random_uniform(UInt32(colors.count-1)))]
         bubble.blendMode = .add
+        bubble.strokeColor = bubble.fillColor
+        bubble.alpha = 0.9
+        bubble.glowWidth = bubble.frame.size.width/2
         animationBackground.addChild(bubble)
-        let startingPoint = CGPoint(x: size.width/CGFloat(arc4random_uniform(10)), y: (-1)*size.height)
+//         let startingPoint = CGPoint(x: CGFloat(arc4random_uniform(UInt32(UIScreen.main.bounds.size.width))), y: (-1)*size.height)
+        let startingPoint = CGPoint(x: CGFloat(arc4random_uniform(UInt32(UIScreen.main.bounds.size.width))), y: (-1)*size.height/2)
+        
+        
+ 
         bubble.position = startingPoint
     }
     
     func floatBubbles() {
         for child in animationBackground.children {
+            
+            let bubble = child as! SKShapeNode
+            
+//            switch bubbleShouldGrow {
+//            case true:
+//                grow(bubble: bubble)
+//            case false:
+//                shrink(bubble: bubble)
+//            }
+            bubble.glowWidth += 0.9
+            bubble.alpha -= 0.1// 0.5 is awesome 
+            
             let xOffset: CGFloat = CGFloat(arc4random_uniform(30)) - 10.0
-            let yOffset: CGFloat = 20.0
+            let yOffset: CGFloat = 40.0
             let newLocation = CGPoint(x: child.position.x + xOffset/2, y: child.position.y + yOffset)
             let moveAction = SKAction.move(to: newLocation, duration: 0.2)
             child.run(moveAction)
         }
     }
+    
+    
     
     func removeExcessBubbles() {
         
---
+++ AnimationIsand/Colors.swift
@@ -0,0 +1,114 @@
+//
+//  Colors.swift
+//  AnimationIsand
+//
+//  Created by Perrin Cloutier on 11/29/17.
+//  Copyright © 2017 Perrin Cloutier. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Colors {
+    
+    struct Custom {
+        
+        struct Gray {
+            static let dark = UIColor.init(red: 49.0/255.0, green: 36.0/255.0, blue: 32.0/255.0, alpha: 0.4)
+        }
+        struct PsychedelicIceCreamShoppe {
+            static let medViolet = UIColor.init(red: 180.0/255.0, green: 172.0/255.0, blue: 216.0/255.0, alpha: 1.0)
+            static let lightViolet = UIColor.init(red: 213.0/255.0, green: 201.0/255.0, blue: 223.0/255.0, alpha: 1.0)
+            static let lightRose = UIColor.init(red: 235.0/255.0, green: 209.0/255.0, blue: 215.0/255.0, alpha: 1.0)
+            static let rose = UIColor.init(red: 242.0/255.0, green: 184.0/255.0, blue: 189.0/255.0, alpha: 1.0)
+            static let lightCoral = UIColor.init(red: 248.0/255.0, green: 146.0/255.0, blue: 134.0/255.0, alpha: 1.0)
+            static let medRose = UIColor.init(red: 255.0/255.0, green: 124.0/255.0, blue: 134.0/255.0, alpha: 1.0)
+            static let darkRose = UIColor.init(red: 219.0/255.0, green: 107.0/255.0, blue: 96.0/255.0, alpha: 1.0)
+            static let brightCoral = UIColor.init(red: 255.0/255.0, green: 89.0/255.0, blue: 97.0/255.0, alpha: 1.0)
+            static let ice = UIColor.init(red: 216.0/255.0, green: 225.0/255.0, blue: 234.0/255.0, alpha: 1.0)
+            static let darkViolet = UIColor.init(red: 156.0/255.0, green: 90.0/255.0, blue: 205.0/255.0, alpha: 1.0)
+            static let lightestBlueSky = UIColor.init(red: 154.0/255.0, green: 216.0/255.0, blue: 234.0/255.0, alpha: 1.0)
+            static let lighterBlueSky = UIColor.init(red: 110.0/255.0, green: 200.0/255.0, blue: 219.0/255.0, alpha: 1.0)
+            static let lightBlueSky = UIColor.init(red: 0/255.0, green: 175.0/255.0, blue: 224.0/255.0, alpha: 1.0)
+            static let deepBlue = UIColor.init(red: 0/255.0, green: 134.0/255.0, blue: 225.0/255.0, alpha: 1.0)
+            static let deepBlueShade = UIColor.init(red: 35.0/255.0, green: 115.0/255.0, blue: 195.0/255.0, alpha: 1.0)
+            static let deepBlueDark = UIColor.init(red: 0/255.0, green: 103.0/255.0, blue: 188.0/255.0, alpha: 1.0)
+            static let lighterBlue = UIColor.init(red: 0/255.0, green: 103.0/255.0, blue: 188.0/255.0, alpha: 1.0)
+            static let neonAqua = UIColor.init(red: 0/255.0, green: 205.0/255.0, blue: 210.0/255.0, alpha: 1.0)
+        }
+        struct VintageSeaStyle {
+            static let brightVintageRed = UIColor.init(red: 220.0/255.0, green: 46.0/255.0, blue: 42.0/255.0, alpha: 1.0)
+            static let lightVintageRed =  UIColor.init(red: 231.0/255.0, green: 93.0/255.0, blue: 47.0/255.0, alpha: 1.0)
+            static let cream = UIColor.init(red: 248.0/255.0, green: 222.0/255.0, blue: 177.0/255.0, alpha: 1.0)
+            static let lightAlgae = UIColor.init(red: 163.0/255.0, green: 188.0/255.0, blue: 156.0/255.0, alpha: 1.0)
+            static let darkAlgae = UIColor.init(red: 3.0/255.0, green: 123.0/255.0, blue: 98.0/255.0, alpha: 1.0)
+            static let medAlgae = UIColor.init(red: 10.0/255.0, green: 144.0/255.0, blue: 117.0/255.0, alpha: 1.0)
+            static let darkAqua = UIColor.init(red: 4.0/255.0, green: 108.0/255.0, blue: 113.0/255.0, alpha: 1.0)
+            static let vintageBlue = UIColor.init(red: 26.0/255.0, green: 123.0/255.0, blue: 146.0/255.0, alpha: 1.0)
+            static let mint = UIColor.init(red: 174.0/255.0, green: 200.0/255.0, blue: 162.0/255.0, alpha: 1.0)
+            static let lightCreamsicle = UIColor.init(red: 255.0/255.0, green: 186.0/255.0, blue: 91.0/255.0, alpha: 1.0)
+            static let darkCreamsicle = UIColor.init(red: 252.0/255.0, green: 126.0/255.0, blue: 31.0/255.0, alpha: 1.0)
+            static let darkNavyBlue = UIColor.init(red: 44.0/255.0, green: 76.0/255.0, blue: 85.0/255.0, alpha: 1.0)
+            static let emeraldGreen = UIColor.init(red: 56.0/255.0, green: 168.0/255.0, blue: 126.0/255.0, alpha: 1.0)
+            static let lemon = UIColor.init(red: 234.0/255.0, green: 221.0/255.0, blue: 54.0/255.0, alpha: 1.0)
+            static let limestone = UIColor.init(red: 174.0/255.0, green: 196.0/255.0, blue: 191.0/255.0, alpha: 1.0)
+            static let lime = UIColor.init(red: 183.0/255.0, green: 181.0/255.0, blue: 61.0/255.0, alpha: 1.0)
+            static let darkGreen = UIColor.init(red: 85.0/255.0, green: 110.0/255.0, blue: 45.0/255.0, alpha: 1.0)
+        }
+        
+    }
+    class func getVintage() -> [UIColor] {
+        //vintage colors set
+        let brightVintageRed = UIColor.init(red: 220.0/255.0, green: 46.0/255.0, blue: 42.0/255.0, alpha: 1.0)
+        let lightVintageRed =  UIColor.init(red: 231.0/255.0, green: 93.0/255.0, blue: 47.0/255.0, alpha: 1.0)
+        let cream = UIColor.init(red: 248.0/255.0, green: 222.0/255.0, blue: 177.0/255.0, alpha: 1.0)
+        let lightAlgae = UIColor.init(red: 163.0/255.0, green: 188.0/255.0, blue: 156.0/255.0, alpha: 1.0)
+        let darkAlgae = UIColor.init(red: 3.0/255.0, green: 123.0/255.0, blue: 98.0/255.0, alpha: 1.0)
+        let medAlgae = UIColor.init(red: 10.0/255.0, green: 144.0/255.0, blue: 117.0/255.0, alpha: 1.0)
+        let darkAqua = UIColor.init(red: 4.0/255.0, green: 108.0/255.0, blue: 113.0/255.0, alpha: 1.0)
+        let vintageBlue = UIColor.init(red: 26.0/255.0, green: 123.0/255.0, blue: 146.0/255.0, alpha: 1.0)
+        let mint = UIColor.init(red: 174.0/255.0, green: 200.0/255.0, blue: 162.0/255.0, alpha: 1.0)
+        let lightCreamsicle = UIColor.init(red: 255.0/255.0, green: 186.0/255.0, blue: 91.0/255.0, alpha: 1.0)
+        let darkCreamsicle = UIColor.init(red: 252.0/255.0, green: 126.0/255.0, blue: 31.0/255.0, alpha: 1.0)
+        let darkNavyBlue = UIColor.init(red: 44.0/255.0, green: 76.0/255.0, blue: 85.0/255.0, alpha: 1.0)
+        let emeraldGreen = UIColor.init(red: 56.0/255.0, green: 168.0/255.0, blue: 126.0/255.0, alpha: 1.0)
+        let lemon = UIColor.init(red: 234.0/255.0, green: 221.0/255.0, blue: 54.0/255.0, alpha: 1.0)
+        let limestone = UIColor.init(red: 174.0/255.0, green: 196.0/255.0, blue: 191.0/255.0, alpha: 1.0)
+        let lime = UIColor.init(red: 183.0/255.0, green: 181.0/255.0, blue: 61.0/255.0, alpha: 1.0)
+        let darkGreen = UIColor.init(red: 85.0/255.0, green: 110.0/255.0, blue: 45.0/255.0, alpha: 1.0)
+        let ice = UIColor.init(red: 216.0/255.0, green: 225.0/255.0, blue: 234.0/255.0, alpha: 1.0)
+        
+        
+    let vintUIColors = [brightVintageRed, lightVintageRed, cream, lightCreamsicle, darkCreamsicle, lightAlgae, medAlgae, darkAlgae, darkAqua,  mint, darkNavyBlue, emeraldGreen, lemon, limestone, lime, darkGreen, vintageBlue, ice]
+    return vintUIColors
+}
+    
+    class func getPsychedelicIceCreamShopColors()-> [UIColor] {
+        
+        
+        // psychedelic ice cream shop
+        let medViolet = UIColor.init(red: 180.0/255.0, green: 172.0/255.0, blue: 216.0/255.0, alpha: 1.0)
+        let lightViolet = UIColor.init(red: 213.0/255.0, green: 201.0/255.0, blue: 223.0/255.0, alpha: 1.0)
+        let lightRose = UIColor.init(red: 235.0/255.0, green: 209.0/255.0, blue: 215.0/255.0, alpha: 1.0)
+        let rose = UIColor.init(red: 242.0/255.0, green: 184.0/255.0, blue: 189.0/255.0, alpha: 1.0)
+        let lightCoral = UIColor.init(red: 248.0/255.0, green: 146.0/255.0, blue: 134.0/255.0, alpha: 1.0)
+        let medRose = UIColor.init(red: 255.0/255.0, green: 124.0/255.0, blue: 134.0/255.0, alpha: 1.0)
+        let darkRose = UIColor.init(red: 219.0/255.0, green: 107.0/255.0, blue: 96.0/255.0, alpha: 1.0)
+        let brightCoral = UIColor.init(red: 255.0/255.0, green: 89.0/255.0, blue: 97.0/255.0, alpha: 1.0)
+        let darkViolet = UIColor.init(red: 156.0/255.0, green: 90.0/255.0, blue: 205.0/255.0, alpha: 1.0)
+        let lightestBlueSky = UIColor.init(red: 154.0/255.0, green: 216.0/255.0, blue: 234.0/255.0, alpha: 1.0)
+        let lighterBlueSky = UIColor.init(red: 110.0/255.0, green: 200.0/255.0, blue: 219.0/255.0, alpha: 1.0)
+        let lightBlueSky = UIColor.init(red: 0/255.0, green: 175.0/255.0, blue: 224.0/255.0, alpha: 1.0)
+        let deepBlue = UIColor.init(red: 0/255.0, green: 134.0/255.0, blue: 225.0/255.0, alpha: 1.0)
+        let deepBlueShade = UIColor.init(red: 35.0/255.0, green: 115.0/255.0, blue: 195.0/255.0, alpha: 1.0)
+        let deepBlueDark = UIColor.init(red: 0/255.0, green: 103.0/255.0, blue: 188.0/255.0, alpha: 1.0)
+        let lighterBlue = UIColor.init(red: 0/255.0, green: 103.0/255.0, blue: 188.0/255.0, alpha: 1.0)
+        let neonAqua = UIColor.init(red: 0/255.0, green: 205.0/255.0, blue: 210.0/255.0, alpha: 1.0)
+        
+        
+        let psyUIColors = [neonAqua, lightCoral, lightViolet, deepBlue, darkViolet, lightestBlueSky, brightCoral, medViolet, medViolet, deepBlueDark, medRose, lightViolet, darkRose, medRose, lighterBlueSky, medViolet, lighterBlue, lightBlueSky, lightRose, rose, deepBlueShade]
+        
+        return psyUIColors
+    }
+    
+}
